f8e45f325f16795bab0eb86fb0f82edfe8f47f91
saranya-kethireddy <saranyakethireddy@gmail.com>
2021-11-18 14:26:27 +0530

    initial commit

---
+++ employeemgmt/src/main/java/dxc/b2/thyme/services/EmployeeService.java
@@ -0,0 +1,19 @@
+package dxc.b2.thyme.services;
+
+import java.util.List;
+
+import dxc.b2.thyme.model.Employee;
+
+public class EmployeeService {
+	 public List < Employee > getAllEmployees() {
+		return null;
+	}
+	 public void saveEmployee(Employee employee) {
+	}
+	public Employee getEmployeeById(long id) {
+		return null;
+	}
+	 public void deleteEmployeeById(long id) {
+	}
+
+}
